f960138c6f4150113440b0404843444cf176207c
wang yuepeng <715484127@qq.com>
2013-08-10 00:59:49 +0800

    Initial Commit

---
+++ UITest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UITest
+//
+//  Created by wang yuepeng on 13-8-10.
+//  Copyright (c) 2013年 wang yuepeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
